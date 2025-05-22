805eb958870f5afbd1a4754e66a9ba5e91ae187b
Thundeeer <zihengloudi@gmail.com>
2025-05-22 15:14:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ def main():
     string = input("Introdueix un string:")
 
     print("Quina transformació vols?")
-    print("[1] Text amb tot majuscules")
-    print("[2] Text amb tot minúscuies")
+    print("[1] Text amb tot majuscules!")
+    print("[2] Text amb tot minúscuies!")
     print("[3] Text capitalitzat ")
 
     opcio = input("opció escollida: ")
